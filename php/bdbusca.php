2c37be5b2243a3c37984b4524160ef302ac2c88c
moraoscar65 <moraoscar65@gmail.com>
2019-12-17 20:50:08 -0600

    navbar y otras cosillas

--- php/bdbusca.php
+++ php/bdbusca.php
@@ -8,14 +8,14 @@ if ($conexion->connect_errno) {
     die('Error en la conexion');
 }
 
-$sql = "SELECT prodID, 	prodNombre, prodDescripcion, prodPrecio, prodExistencias, prodImagen
-FROM producto WHERE prodID = ?";
+$sql = "SELECT id, imagen, nombre, precio, tipo, descripcion, Cantidad
+FROM productos_1 WHERE id = ?";
 
 $stmt = $conexion->prepare($sql);
 $stmt->bind_param("s", $_GET['q']);
 $stmt->execute();
 $stmt->store_result();
-$stmt->bind_result($prodID, $prodNombre, $prodDescripcion, $prodPrecio, $prodExistencias, $prodImagen);
+$stmt->bind_result($prodID, $prodImagen, $prodNombre, $prodPrecio, $prodTipo, $prodDescripcion, $prodExistencias);
 $stmt->fetch();
 $stmt->close();
 if($prodID==$_GET['q']){
@@ -24,6 +24,7 @@ echo "<table class='table'>";
 echo "<tr class='info'>";
 echo "<th>ID</th>";
 echo "<th>NOMBRE</th>";
+echo "<th>TIPO</th>";
 echo "<th>DESCRIPCION</th>";
 echo "<th>PRECIO</th>";
 echo "<th>EXISTENCIAS</th>";
@@ -32,10 +33,11 @@ echo "</tr>";
 echo "<tr class='active'>";
 echo "<td>".$prodID."</td>";
 echo "<td>".$prodNombre."</td>";
+echo "<td>".$prodTipo."</td>";
 echo "<td>".$prodDescripcion."</td>";
 echo "<td>$".$prodPrecio."</td>";
 echo "<td>".$prodExistencias." Unidades</td>";
-echo "<td> <img class='img-rounded' src='".$prodImagen."'> ruta: <b>".$prodImagen."</b></td>";
+echo "<td> <img class='img-rounded' src='".$prodImagen."'></td>";
 echo "</tr>";
 echo "</table>";
 }
